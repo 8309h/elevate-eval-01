abf0719d9d7242b9f32bf5296ff251d24c967750
harshal <harshalwagh201718@gmail.com>
2026-05-23 19:05:15 +0530

    added course data

---
+++ course_dashboard/src/data/courses.js
@@ -0,0 +1,57 @@
+const courses = [
+      {
+            id: 1,
+            title: "React Basics",
+            category: "Frontend",
+            duration: "6 Weeks",
+            level: "Begineer",
+            price: "2999",
+            description: "Learn React"
+      },
+      {
+            id: 2,
+            title: "Node Basics",
+            category: "Backend",
+            duration: "6 Weeks",
+            level: "Begineer",
+            price: "1559",
+            description: "Learn Node.js"
+      },
+      {
+            id: 3,
+            title: "AI for Developer",
+            category: "AI",
+            duration: "4 Weeks",
+            level: "Intermediate",
+            price: "1999",
+            description: "Learn Basics of AI and its intergration "
+      },
+      {
+            id: 4,
+            title: "Backend Developemnt",
+            category: "Backend",
+            duration: "8 Weeks",
+            level: "Advanced",
+            price: "2999",
+            description: "Learn Backend Developemnt"
+      },
+      {
+            id: 5,
+            title: "DSA",
+            category: "Data",
+            duration: "4 Weeks",
+            level: "Begineer",
+            price: "2999",
+            description: "Learn DSA"
+      },
+      {
+            id: 6,
+            title: "AI/ML",
+            category: "AI",
+            duration: "6 Weeks",
+            level: "Advanced",
+            price: "2999",
+            description: "Learn AI/ML"
+      }
+]
+export default courses;
